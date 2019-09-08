8685e9ff37fe4f5a50566cf269457d9942d9e731
idwxy <idwxy@live.com>
2019-09-08 13:35:59 +0800

    first commit

---
+++ src/main/java/com/idwxy/fli/FliApplication.java
@@ -0,0 +1,13 @@
+package com.idwxy.fli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FliApplication.class, args);
+	}
+
+}
